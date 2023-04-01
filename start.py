6ae400b47145c02ae5402ece411a07fd13fabac2
Patrick Amsler <patrick.amsler@abraxas.ch>
2023-04-01 12:29:44 +0200

    logging added

--- start.py
+++ start.py
@@ -1,28 +1,37 @@
-from config import load_config
+from util.config import load_config
 from wireless.wifi import Wifi
 from sensor.sht3x import SHT3X
 from umqtt.simple import MQTTClient
 from machine import Pin, I2C, Timer
+import util.logging as logging
+import sys
 
+def init_logging():
+    # logging.basicConfig(filename='log-file.log', level=logging.DEBUG, filemode='a') # log to file
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout) # log to console
+    for handler in logging.getLogger().handlers:
+        handler.setFormatter(logging.Formatter("[%(levelname)s] - %(name)s - %(message)s"))
 
 def main():
+    init_logging()
+    log = logging.getLogger(__name__)
     config = load_config()
 
-    # initialize sensor
+    log.info("initializing SHT31 sensor")
     sht31 = SHT3X(
         i2c=I2C(0, scl=Pin(1), sda=Pin(0), freq=100000)
     )
     if not sht31.isConnected():
         raise RuntimeError("SHT31 not found on I2C bus")
 
-    # connect to WiFi
+    log.info("initializing WiFi")
     wifi = Wifi(
         ssid=config['wlan.ssid'],
         password=config['wlan.password']
     )
     wifi.connect()
 
-    # initialize mqtt client
+    log.info("initializing MQTT client")
     mqtt_temperature_topic = config["mqtt.topics.status"]
     mqtt_clientId = config["mqtt.clientId"]
     mqtt_client = MQTTClient(
@@ -35,12 +44,12 @@ def main():
     # main data acquisition an publishing loop
     def publish_data(timer):
         if not wifi.is_connected():
-            print("WiFi not connected. Not publishing data")
+            log.info("WiFi not connected. Not publishing data")
             return
 
         temp, hum, error = sht31.read()
         if error:
-            print("Error reading sensor. Not publishing data")
+            log.error("Error reading sensor data")
             return
 
         payload = {
@@ -50,19 +59,19 @@ def main():
         }
 
         if not error:
-            print("Publishing data...", payload)
+            log.debug("Publishing data: " + str(payload))
             try:
                 mqtt_client.connect()
                 mqtt_client.publish(mqtt_temperature_topic, str(payload), retain=False, qos=0)
                 mqtt_client.disconnect()
-                print("Data published successfully")
+                log.debug("Data published")
             except Exception as e:
-                print("Error publishing data:", e)
+                log.error("Error publishing data: ", e)
 
-
-    # start the timer for the main loop
+    log.info("starting data acquisition loop")
     timer = Timer()
     timer.init(period=20000, mode=Timer.PERIODIC, callback=publish_data)
+
 
 # start the main function
 if __name__ == "__main__":
